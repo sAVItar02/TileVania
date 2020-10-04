0b441a0a53130ade42e43d10d78be059c2022ea9
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-10-04 17:49:48 +0530

    Implemented Jumping
    (Buggy)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,29 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
@@ -24,6 +35,20 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 velocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = velocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; };
+
+        if(CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            myRigidbody.velocity += jumpVelocity;
+        }
     }
 
     private void FlipSprite()
